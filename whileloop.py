e8d2a83f795c0d9e91d63c69ed977b982f539f96
twinklechhillar <twinklechhillar89@gmail.com>
2026-07-07 08:48:43 +0530

    Create whileloop.py

---
+++ whileloop.py
@@ -0,0 +1,36 @@
+print num from 1 to 10
+i =1
+while i<=10:
+    print(i)
+    i +=1
+
+print num from 10 to 1
+  i =10
+while i>=1:
+   print(i)
+   i-=1
+  
+ print even num
+i=2
+while i<=20:
+    print(i)
+    i+=2
+
+print odd num 
+i=1
+while i<=20:
+    print(i)
+    i+=2
+
+print sum of n num 
+n = int(input("enter a num:"))
+
+i=1
+total =0
+
+while i<=n:
+    total = total +i
+    i+=1
+    
+print(total) 
+
