f69fe5faf39073741430be2ef6f7e1ff5b8e1bfc
twinklechhillar <twinklechhillar89@gmail.com>
2026-07-08 15:58:24 +0530

    Create break.java

---
+++ break.java
@@ -0,0 +1,17 @@
+print num from 1 to 10 and break at 7
+
+i =1
+while i<=10:
+    if i==7:
+        break
+    print(i)
+    i +=1
+
+
+print num 20 to 1 break at 7
+  i = 20
+while i>=1:
+    if i==7:
+        break
+    print(i)
+    i-=1
