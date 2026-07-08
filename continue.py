2a0549b747ff7afdc3ac3a7c5a2c2b9c2084ff50
twinklechhillar <twinklechhillar89@gmail.com>
2026-07-08 16:10:20 +0530

    Create continue.py

---
+++ continue.py
@@ -0,0 +1,18 @@
+print num from 1 to 10 skipping 10
+i = 1
+while i<=20:
+    if i==10:
+        i+=1
+        continue
+    print(i)
+    i+=1
+
+
+print all even num skipping 12
+  i =2
+while i<=20:
+    if i==12:
+        i+=2
+        continue
+    print(i)
+    i+=2
